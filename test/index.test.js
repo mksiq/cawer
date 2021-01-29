0b83bddc92c3dd8afdc99b5c4dc3180c2505b479
root <root@LAPTOP-LKOK4UDA.localdomain>
2021-01-29 16:32:33 -0500

    update tests

--- test/index.test.js
+++ test/index.test.js
@@ -12,35 +12,90 @@ test("Api is running: must return welcome message", () => {
 });
 
 test("Insert user that exists: not allowed", () => {
-  const theDate = `username=tester&password=testertester&email=tester@gmail.com&alias=tester`;
+  const data = `username=tester&password=testertester&email=tester@gmail.com&alias=tester`;
   return fetch("http://localhost:8080/register-user", {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
     },
-    body: theDate,
+    body: data,
   }).then((res) => {
-    res.json().then((x) => {}).catch(x => {
-      expect(x.message).toBe("User already exists")
+    res.json().then((x) => {
+      expect(x.message).toEqual("User already exists");
     })
-  }).catch(x => {
-    expect(x.message).toBe("User already exists");
   })
 });
 
-test("Insert user that does not exists: allowed", () => {
-  const newUser = `username=new&password=newnewnew&email=new@gmail.com&alias=new`;
+test("Insert user that does not exists must throw no errors", () => {
+  const data = `username=newnew&password=aaaaaaaaa&email=newnew@gmail.com&alias=newnew`;
   return fetch("http://localhost:8080/register-user", {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
     },
-    body: newUser,
+    body: data,
   }).then((res) => {
-    res.json().then((x) => {expect(x.username).toBe("new"); done();} ).catch(x => {
-      expect(x).not.toThrow(x)
-    }).catch(x => {
-      expect(x.message).not.toBe("User already exists");
-    });
+    return res.json().then((x) => {
+      expect(x.username).toEqual("newnew");
+    }).catch(error => {
+      fail('f')
+    })
+  }).catch( err => {fail('f')})
+});
+
+test("User login: matching username and  password must login", () => {
+  const loggingUser = `password=testertester&username=tester`;
+  return fetch("http://localhost:8080/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+    },
+    body: loggingUser,
+  }).then((res) => {
+    return  res.json().then((x) => {
+      expect(x.message).not.toEqual("Password does not match");
+    }).catch(error => {
+      fail('Wrong password');
+    })
+  }).catch( err => {
+    fail('Something else went wrong');
+  })
+});
+
+test("User login: Wrong username and  password must  not login", () => {
+  const loggingUser = `password=atester&username=tester`;
+  return fetch("http://localhost:8080/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+    },
+    body: loggingUser,
+  }).then((res) => {
+    return  res.json().then((x) => {
+      expect(x.message).toEqual("Password does not match");
+    }).catch(error => {
+      fail('Wrong password');
+    })
+  }).catch( err => {
+    fail('Something else went wrong');
+  })
+});
+
+test("User login: Non existing username must throw error", () => {
+  const loggingUser = `password=atester&username=nope`;
+  return fetch("http://localhost:8080/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+    },
+    body: loggingUser,
+  }).then((res) => {
+    return  res.json().then((x) => {
+      expect(x.message).toEqual("User not found");
+    }).catch(error => {
+      fail('Did not give not found user error');
+    })
+  }).catch( err => {
+    fail('Something else went wrong');
   })
 });
